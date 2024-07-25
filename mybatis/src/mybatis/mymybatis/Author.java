b4f4c3a5a52e6761156394eeef5f391d895623d0
jeong jaehun <145264424+ha-dong@users.noreply.github.com>
2024-07-25 17:23:20 +0900

    Add files via upload

---
+++ mybatis/src/mybatis/mymybatis/Author.java
@@ -0,0 +1,10 @@
+package mybatis.mymybatis;
+
+public class Author {
+	private Author id;
+	private Author username;
+	private Author password;
+	private Author email;
+	private Author bio;
+	private Author favouriteSection;
+}
